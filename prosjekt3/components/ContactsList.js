2115df46dd443c39a04a060c23bb374c5846811e
Tvedt1 <tvedtenb@gmail.com>
2018-10-10 15:21:38 +0200

    #40, retrieved activity data on homescreen.

--- prosjekt3/components/ContactsList.js
+++ prosjekt3/components/ContactsList.js
@@ -7,6 +7,7 @@ import {
     Alert,
     FlatList,
     Button,
+    Image,
 } from 'react-native'
 
 import {
@@ -21,19 +22,19 @@ const ContactsList = ({ addedContacts = [], importedContacts = [], handleContact
         return (
             <List>
                 <FlatList
-                data={contacts}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <ListItem
-                    containerStyle={{padding: 20}}
-                    roundAvatar
-                    title={`${item.firstName} ${item.lastName}`}
-                    avatar={item.imageAvailable ? {uri: item.image.uri} : require('../assets/images/profile.png')}
-                    onPress={() => handleContactPress(item)}
-                    // Only imported contacts has a lookupKey, so that's an easy way to check which type the contact is
-                    onLongPress={item.lookupKey ? () => Alert.alert("Can't do anything with phone contact") : () => handleDelete(item)}
-                    />
-                )}
+                    data={contacts}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                        <ListItem
+                            containerStyle={{ padding: 20 }}
+                            roundAvatar
+                            title={`${item.firstName} ${item.lastName}`}
+                            avatar={item.imageAvailable ? { uri: item.image.uri } : require('../assets/images/profile.png')}
+                            onPress={() => handleContactPress(item)}
+                            // Only imported contacts has a lookupKey, so that's an easy way to check which type the contact is
+                            onLongPress={item.lookupKey ? () => Alert.alert("Can't do anything with phone contact") : () => handleDelete(item)}
+                        />
+                    )}
                 />
             </List>
         )
@@ -44,38 +45,38 @@ const ContactsList = ({ addedContacts = [], importedContacts = [], handleContact
         return (
             <View style={styles.contactsContainer}>
                 <View style={styles.contactsHeader}>
-                <Text h4>Added contacts:</Text>
-                <Button title="Add Contact" onPress={addContact}/>
+                    <Text h4>Added contacts:</Text>
+                    <Button title="Add Contact" onPress={addContact} />
                 </View>
                 {listContacts(addedContacts)}
                 <View style={styles.contactsHeader}>
-                <Text h4>Imported contacts:</Text>
-                {
-                    importedContacts.length == 0 && <Button title="Import" onPress={importContacts}/>
-                }
+                    <Text h4>Imported contacts:</Text>
+                    {
+                        importedContacts.length == 0 && <Button title="Import" onPress={importContacts} />
+                    }
                 </View>
                 {listContacts(importedContacts)}
             </View>
         )
     } else {
         // Show an empty contact list
-        return(
-            <View style={[styles.welcomeContainer, {alignItems: "center"}]}>
+        return (
+            <View style={[styles.welcomeContainer, { alignItems: "center" }]}>
                 <Image
-                source={
-                    require('../assets/images/alone.gif')
-                }
-                style={styles.welcomeImage}
+                    source={
+                        require('../assets/images/alone.gif')
+                    }
+                    style={styles.welcomeImage}
                 />
 
                 <View style={styles.getStartedContainer}>
-                <Text style={styles.getStartedText}>It looks like your contacts list is empty.</Text>
-                <View style={styles.buttonContainer}>
-                    <Button title="Import from phone" onPress={importContacts}/>
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button title="Add Contact" onPress={addContact}/>
-                </View>
+                    <Text style={styles.getStartedText}>It looks like your contacts list is empty.</Text>
+                    <View style={styles.buttonContainer}>
+                        <Button title="Import from phone" onPress={importContacts} />
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <Button title="Add Contact" onPress={addContact} />
+                    </View>
                 </View>
 
             </View>
